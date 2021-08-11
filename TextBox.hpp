dccc3a994ea40f9fb50bdfd969705276588347a5
Hayk Khachatryan <hayk.g.khachatryan@gmail.com>
2021-08-11 22:42:25 +0400

    added list of methods and variables for class TextBox

--- TextBox.hpp
+++ TextBox.hpp
@@ -3,11 +3,19 @@
 #include <string>
 using namespace std;
 using namespace sf;
-class TextBox : Drawable
+class TextBox : public Drawable
 {
+	int dif = 0;
+	bool is_selected = 0;
 	Text text;
 	Font font;
-	Sprite background;
+	Sprite background, background_sel;
+	
+	TextBox(Sprite im, Sprite im_sel);
+	TextBox(Sprite im, bool is_same_image = 0);
+	TextBox(Sprite im, IntRect im_sel);
+	void select();
+	void unselect();
 	void proceed_char(char ch);
 	void set_text_color(Color col);
 	void set_text(string text);
@@ -18,3 +26,7 @@ class TextBox : Drawable
 
 	}
 };
+
+
+TextBox createTextBox(Texture& texture, Vector2f position, IntRect texture_rect_def, IntRect texture_rect_selected);
+TextBox createTextBox(Texture& texture, Vector2f position, IntRect texture_rect_def);
